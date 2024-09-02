5f144a7202d2f8abbece5d38d8a0d5b3da21148d
Vinicius Sevilha <vd.sevilha@gmail.com>
2024-09-02 07:47:46 -0300

    UPD: database connection

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,21 +3,31 @@ const sqlite3 = require('sqlite3').verbose();
 
 export class Database {
 
-    private databaseName: any;
-    private db: any;
+  private databaseName: string;
+  private database: string;
+  private db: any;
 
-    constructor(database: string) {
-        this.databaseName = database;
+  constructor({database, databaseName}) {
+    this.databaseName = database;
+    this.databaseName = databaseName;
+  }
+
+  connection() {
+    switch (this.database) {
+      case "mongo" : return "not yet implemented";
+      case "sqlite": return this.sqliteConnection();
+      default: return this.sqliteConnection();
     }
+  }
 
-    connection() {
-        this.db = new sqlite3.Database(`../../db/${this.databaseName}.db`, sqlite3.OPEN_READWRITE, (err) => {
-          if (err) {
-            console.error(err.message);
-          }
-          console.log('Connected to the database.');
-        });
+  private sqliteConnection() {
+    this.db = new sqlite3.Database(`../../db/${this.databaseName}.db`, sqlite3.OPEN_READWRITE, (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Connected to the database.');
+    });
 
-        return this.db;
-    }
+    return this.db;
+  }
 }
