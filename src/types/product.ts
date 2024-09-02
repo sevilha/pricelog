0d2c12651c71666f38357a066c9675094e3dcfe9
Vinicius Sevilha <vd.sevilha@gmail.com>
2024-09-02 07:47:16 -0300

    UPD: product interface

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,16 +1,32 @@
 export interface Product {
     id: string;
-    type: ProductType;
     code: string;
+    name: string;
+    type: ProductType;
+    segment: string;
+    segmentType: string;
 }
 
 export type ProductType = "FII" | "AÇÕES";
 
-export interface ValuationHistory {
+export interface AdministrationFee {
+    id: string;
+    fee: number;
+    billingCycle: BillingCycle;
     product: Product;
-    price: number;
+}
+
+export type BillingCycle = "A.A." | "A.M." | "A.S." | "A.Q." | "A.T." | "A.Sm."
+
+export interface ValuationHistory {
+    id: string;
+    createAt: string;
     dy: number;
     dailyLiquidity: number;
+    equityValue: string;
+    issuedQuotas: number;
+    price: number;
+    product: Product;
     pvp: number;
-    date: string;
+
 }
