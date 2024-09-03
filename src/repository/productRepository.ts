458851a9d40f26e211ec51eab7d4279ed14f9ce4
Vinicius Sevilha <vd.sevilha@gmail.com>
2024-09-03 07:31:57 -0300

    CHG: update product repository

--- src/repository/productRepository.ts
+++ src/repository/productRepository.ts
@@ -1,8 +1,14 @@
+import { DB } from "../utils/database";
+
 export class ProductRepository {
     
-    private dbConnection;
-    
+    private dbConnection: DB;
+
     constructor({dbConnection}) {
         this.dbConnection = dbConnection;    
+    }
+
+    private async find(query: string) {
+        return await this.dbConnection.find(query);
     }
 }
